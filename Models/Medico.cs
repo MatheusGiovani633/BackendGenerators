298c1fd9187f10b1c8fc9ead5a72aed4482e7f01
MatheusGiovani633 <MatheusGiovani633>
2025-11-10 08:29:34 -0300

    Fix(Retirando adapts restantes do projeto)

--- Models/Medico.cs
+++ Models/Medico.cs
@@ -1,7 +1,6 @@
 
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using BackendGenerators.Models.Dtos;
 namespace BackendGenerators.Models
 {
     public class Medico 
@@ -18,34 +17,6 @@ namespace BackendGenerators.Models
 
         [Required]
         public int CRM { get; set; }
-        internal object Adapt<T>()
-        {
-            if (typeof(T) == typeof(MedicoDto))
-            {
-                var dto = new MedicoDto
-                {
-                    CRM = CRM,
-                    Identificador = Pessoa.Identificador,
-                    Nome = Pessoa.Nome,
-                    Email = Pessoa.Email,
-                    Telefone = Pessoa.Telefone,
-                    Celular = Pessoa.Celular,
-                    Celular2 = Pessoa.Celular2,
-                    DataNascimento = Pessoa.DataNascimento,
-                    Cpf = Pessoa.Cpf,
-                    Sexo = Pessoa.Sexo,
-                    Cep = Pessoa.Cep,
-                    Logradouro = Pessoa.Logradouro,
-                    Numero = Pessoa.Numero,
-                    Cidade = Pessoa.Cidade,
-                    Estado = Pessoa.Estado,
-                    Complemento = Pessoa.Complemento,
-                    Bairro = Pessoa.Bairro,
-                };
-                return (T)(object)dto;
-            }
-            throw new NotImplementedException($"Adaptation to {typeof(T).Name} is not implemented.");
-        }
     }
     
 }
